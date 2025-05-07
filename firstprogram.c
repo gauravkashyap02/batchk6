13bd6aaeac9955223a5acd7c170f90d8f8a00e32
Gaurav Kashyap <gauravkp656@gmail.com>
2025-05-07 17:10:14 +0530

    First commit

---
+++ firstprogram.c
@@ -0,0 +1,7 @@
+#include<stdio.h>  //#include-->preprocessor <stdio.h>--->header file ---> Library --->printf or scanf function
+int main(){        //int --> return type main()-Code compile or execution start
+
+printf("Welcome to C Programming");
+
+return 0;    //return 0--->code successfully compile and execute
+}
---
+++ helloworld.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Welcome to Coding Age");
+    return 0;
+}
